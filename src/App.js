484b818f35a29b6dafdd76e25726a2946353c617
madeinchema <chemavg1@gmail.com>
2020-11-17 22:27:03 +0100

    Installed and tested Ant Design and React Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import { Button } from 'antd';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <Switch>
+        <Route exact path='/'>
+          Home
+          <Link to='/cryptos'>
+            <Button>Cryptos</Button>
+          </Link>
+        </Route>
+        <Route exact path='/cryptos'>
+          Cryptos
+          <Link to='/'>
+            <Button>Home</Button>
+          </Link>
+        </Route>
+        <Route path='*'>
+          404
+          <Link to='/'>
+            <Button>Home</Button>
+          </Link>
+        </Route>
+      </Switch>
+
+    </Router>
   );
 }
 
