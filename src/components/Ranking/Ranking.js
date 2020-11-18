2367c7c5932a4745e60654f1a801ff5444226f0a
madeinchema <chemavg1@gmail.com>
2020-11-18 14:57:38 +0100

    Added sorting to the Table and improved the styles.

--- src/components/Ranking/Ranking.js
+++ src/components/Ranking/Ranking.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Table } from 'antd';
+import './Ranking.css';
 import { getCoinsRanking } from '../../utilities/api';
 
 const columns = (currency) => {
@@ -8,11 +9,12 @@ const columns = (currency) => {
       title: '#',
       dataIndex: 'market_cap_rank',
       key: 'market_cap_rank',
+      sorter: (a, b) => a.market_cap_rank - b.market_cap_rank,
     },
     {
       title: 'Coin',
-      dataIndex: 'symbol',
-      key: 'symbol',
+      dataIndex: 'id',
+      key: 'id',
       render: (symbol, coin) => (
         <div style={{ display: 'flex', alignContent: 'center'}}>
           <img
@@ -25,7 +27,7 @@ const columns = (currency) => {
         </span>
 
           <span style={{ fontWeight: '600', color: '#8c8c8c' }}>
-          {symbol.toUpperCase()}
+          {coin.symbol.toUpperCase()}
         </span>
 
 
@@ -36,18 +38,21 @@ const columns = (currency) => {
       title: 'Price',
       dataIndex: 'current_price',
       key: 'current_price',
-      render: price => `${price} ${currency.toUpperCase()}`,
+      sorter: (a, b) => a.current_price - b.current_price,
+      render: price => `${price.toLocaleString()} ${currency.toUpperCase()}`,
     },
     {
       title: '24h Change',
       dataIndex: 'price_change_percentage_24h',
       key: 'price_change_percentage_24h',
+      sorter: (a, b) => a.price_change_percentage_24h - b.price_change_percentage_24h,
       render: change => `${change ? change.toFixed(2) : 0} %`,
     },
     {
       title: 'Market Cap',
       dataIndex: 'market_cap',
       key: 'market_cap',
+      sorter: (a, b) => a.market_cap - b.market_cap,
       render: marketCap => `${marketCap.toLocaleString()} ${currency.toUpperCase()}`,
     },
     {
@@ -58,7 +63,7 @@ const columns = (currency) => {
         // TODO: Improve regex
         let id = image.replace(/(https:\/\/assets.coingecko.com\/coins\/images\/)/, '');
         id = id.replace(/(?!\d).+/, '');
-        return <img style={{ height: 28 }} alt='coin icon' src={`https://www.coingecko.com/coins/${id}/sparkline`}/>
+        return <img style={{ height: 48 }} alt='coin icon' src={`https://www.coingecko.com/coins/${id}/sparkline`}/>
       },
     },
   ];
@@ -83,6 +88,7 @@ const Ranking = ({ currency = 'usd' }) => {
   return (
     <div>
       <Table
+        className='rankingTable'
         loading={loading || !cryptosData}
         dataSource={cryptosData}
         columns={columns(currency)}
@@ -94,3 +100,4 @@ const Ranking = ({ currency = 'usd' }) => {
 };
 
 export default Ranking;
+
