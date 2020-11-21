46d3dba608e4dcbe051a4d382710f06ef83350a8
madeinchema <chemavg1@gmail.com>
2020-11-21 22:32:41 +0100

    Completed Circulating Supply Popover. Cleaned code.

--- src/components/Ranking/Ranking.js
+++ src/components/Ranking/Ranking.js
@@ -63,38 +63,7 @@ const columns = (currency) => {
       dataIndex: 'circulating_supply',
       key: 'circulating_supply',
       sorter: (a, b) => a.circulating_supply - b.circulating_supply,
-      render: (circulatingSupply, coin) => (
-        <>
-          <div>
-            {coin.total_supply && (
-              <Popover content={(
-                <Space direction='vertical'>
-                  <Row justify='space-between' gutter={24}>
-                    <Col><Text strong>Circulating Supply</Text></Col>
-                    <Col>{circulatingSupply.toLocaleString()}{' '}{coin.symbol.toUpperCase()}</Col>
-                  </Row>
-                  <Row justify='space-between' gutter={24}>
-                    <Col><Text strong>Max Supply</Text></Col>
-                    <Col>{coin.total_supply.toLocaleString()}{' '}{coin.symbol.toUpperCase()}</Col>
-                  </Row>
-                </Space>
-              )}>
-                <InfoCircleOutlined />{' '}
-              </Popover>
-            )}
-            {circulatingSupply.toLocaleString()}{' '}{coin.symbol.toUpperCase()}
-          </div>
-          {coin.total_supply && (
-            <div>
-              <Progress
-                showInfo={false}
-                percent={(circulatingSupply * 100 / coin.total_supply).toFixed(2)}
-                strokeColor='gray'
-              />
-            </div>
-          )}
-        </>
-      )
+      render: (circulatingSupply, coin) => <CirculatingSupplyColumn coin={coin}/>
     },
     {
       title: 'Last 7 Days',
@@ -110,6 +79,62 @@ const columns = (currency) => {
   ];
 }
 
+function CirculatingSupplyColumn({ coin }) {
+  return (
+    <div>
+      <div>
+        {coin.total_supply && <CirculatingSupplyPopover coin={coin}/>}
+        {coin.circulating_supply.toLocaleString()}{' '}{coin.symbol.toUpperCase()}
+      </div>
+      {coin.total_supply && (
+        <div>
+          <Progress
+            showInfo={false}
+            percent={(coin.circulating_supply * 100 / coin.total_supply).toFixed(2)}
+            strokeColor='gray'
+          />
+        </div>
+      )}
+    </div>
+  )
+}
+
+function CirculatingSupplyPopover({ coin }) {
+
+  const getSupplyPercentage = (circulatingSupply, totalSupply) => {
+    const percentage = (circulatingSupply * 100) / totalSupply;
+    return percentage.toFixed(2);
+  }
+
+  return (
+    <Popover content={(
+      <Space direction='vertical'>
+        <Row justify='space-between' gutter={24}>
+          <Col><Text strong>Percentage</Text></Col>
+          <Col>{getSupplyPercentage(coin.circulating_supply, coin.total_supply)}%</Col>
+        </Row>
+        <div>
+          <Progress
+            showInfo={false}
+            percent={(coin.circulating_supply * 100 / coin.total_supply).toFixed(2)}
+            strokeColor='gray'
+          />
+        </div>
+        <Row justify='space-between' gutter={24}>
+          <Col><Text strong>Circulating Supply</Text></Col>
+          <Col>{coin.circulating_supply.toLocaleString()}{' '}{coin.symbol.toUpperCase()}</Col>
+        </Row>
+        <Row justify='space-between' gutter={24}>
+          <Col><Text strong>Max Supply</Text></Col>
+          <Col>{coin.total_supply.toLocaleString()}{' '}{coin.symbol.toUpperCase()}</Col>
+        </Row>
+      </Space>
+    )}>
+      <InfoCircleOutlined />{' '}
+    </Popover>
+  )
+}
+
 const Ranking = ({ currency = 'usd' }) => {
   const [cryptosData, setCryptosData] = useState(null);
   const [loading, setLoading] = useState(null);
@@ -123,7 +148,7 @@ const Ranking = ({ currency = 'usd' }) => {
         setCryptosData(data)
       });
       console.log(cryptosData)
-    }, 1000)
+    }, 200)
     setLoading(false)
 
   }, [])
