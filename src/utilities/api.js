c53e42b473f4039119cb171e8cc1226dc86174de
madeinchema <chemavg1@gmail.com>
2020-11-23 07:01:25 +0100

    getCoinsRanking now also gets the 7d price change percentage.

--- src/utilities/api.js
+++ src/utilities/api.js
@@ -1,4 +1,4 @@
 export const getCoinsRanking = (currency) => {
-  return fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}`)
+  return fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&price_change_percentage=24h,7d`)
     .then(res => res.json());
 };
