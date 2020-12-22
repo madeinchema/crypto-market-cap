e5479a6f97f889cfb1ec2c1a02469c2ee615e9b7
madeinchema <chemavg1@gmail.com>
2020-12-22 22:43:09 +0100

    add prop-types warn rule to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,5 +27,6 @@ module.exports = {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'warn',
   },
 };
