b3775d24ee2f49b4efc1cfd71c80fa57fdee50c6
madeinchema <chemavg1@gmail.com>
2020-12-25 00:32:27 +0100

    use styled-components in PriceChangeColumn

--- src/components/Ranking/components/PriceChangeColumn.jsx
+++ src/components/Ranking/components/PriceChangeColumn.jsx
@@ -1,24 +1,23 @@
+// eslint-disable-next-line no-unused-vars
+import styled, { css } from 'styled-components/macro';
 import PropTypes from 'prop-types';
 
 function PriceChangeColumn({ children, change }) {
   const hasPriceChangedNegatively = change < 0;
   const hasPriceChangedPositively = change > 0;
-  const hasPriceChanged = change !== 0;
 
   const redColor = '#cf1322';
   const grayColor = '#8c8c8c';
   const greenColor = '#389e0d';
 
-  let priceChangeColor;
-  if (!hasPriceChanged) {
-    priceChangeColor = grayColor;
-  } else if (hasPriceChangedNegatively) {
+  let priceChangeColor = grayColor;
+  if (hasPriceChangedNegatively) {
     priceChangeColor = redColor;
   } else if (hasPriceChangedPositively) {
     priceChangeColor = greenColor;
   }
 
-  return <div style={{ color: priceChangeColor }}>{children}</div>;
+  return <div css={{ color: priceChangeColor }}>{children}</div>;
 }
 
 PriceChangeColumn.propTypes = {
