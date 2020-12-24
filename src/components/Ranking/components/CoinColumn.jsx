66f792d0c57d1e5207604526d16dcf4a71e5adb2
madeinchema <chemavg1@gmail.com>
2020-12-24 20:39:34 +0100

    replace CoinColumn inline styles with obj variables

--- src/components/Ranking/components/CoinColumn.jsx
+++ src/components/Ranking/components/CoinColumn.jsx
@@ -1,17 +1,20 @@
 import PropTypes from 'prop-types';
 
+const coinColumnStyles = {
+  container: { display: 'flex', alignContent: 'center' },
+  image: { width: 24, height: 24, marginRight: 8 },
+  name: { fontWeight: '700', marginRight: 4 },
+  symbol: { color: '#8c8c8c' },
+};
+
 const CoinColumn = (props) => {
   const { coin } = props;
   const { name, image, symbol } = coin;
   return (
-    <div style={{ display: 'flex', alignContent: 'center' }}>
-      <img
-        alt={`${name} icon`}
-        src={image}
-        style={{ width: 24, height: 24, marginRight: 8 }}
-      />
-      <span style={{ fontWeight: '700', marginRight: 4 }}>{name}</span>
-      <span style={{ color: '#8c8c8c' }}>{symbol.toUpperCase()}</span>
+    <div style={coinColumnStyles.container}>
+      <img alt={`${name} icon`} src={image} style={coinColumnStyles.image} />
+      <span style={coinColumnStyles.name}>{name}</span>
+      <span style={coinColumnStyles.symbol}>{symbol.toUpperCase()}</span>
     </div>
   );
 };
