f8e3d367102f196f9e96fe67f7d854680533419a
madeinchema <chemavg1@gmail.com>
2020-12-26 13:13:32 +0100

    update dynamic-heading styles structure

--- src/pages/home/components/DynamicHeading/DynamicHeading.jsx
+++ src/pages/home/components/DynamicHeading/DynamicHeading.jsx
@@ -5,30 +5,35 @@ import './dynamic-heading.scss';
 
 const { Title, Paragraph } = Typography;
 
-const heading = `Today's Cryptocurrency Prices by Market Cap`;
-
-const subheading = `The global crypto market cap is $671.42B, a 3.64% increase over the last day.`;
-
-const article = `The total crypto market volume over the last 24 hours is $193.24B, which makes a 14.02% increase. The total volume in DeFi is currently $4.83B, 2.50% of the total crypto market 24-hour volume. The volume of all stable coins is now $75.16B, which is 38.90% of the total crypto market 24-hour volume. Bitcoin's price is currently $24,901.73. Bitcoin’s dominance is currently 68.90%, an increase of 0.90% over the day.`;
-
 const DynamicHeading = () => {
   const [isReadMoreOpen, setIsReadMoreOpen] = useState(undefined);
 
   return (
     <>
-      <Title level={2} className="dynamic-heading__title">
-        {heading}
+      <Title level={2} className="title">
+        Today&apos;s Cryptocurrency Prices by Market Cap
       </Title>
-      <div className="dynamic-heading__content">
-        <Paragraph>
-          {subheading}{' '}
+      <div className="content">
+        <Paragraph className="content--text">
+          The global crypto market cap is $671.42B, a 3.64% increase over the
+          last day.{' '}
           <ReadMore
             isOpen={isReadMoreOpen}
             setIsOpen={setIsReadMoreOpen}
-            className="dynamic-heading__read-more"
+            className="read-more"
           />
         </Paragraph>
-        {isReadMoreOpen && <Paragraph>{article}</Paragraph>}
+        {isReadMoreOpen && (
+          <Paragraph className="content--text">
+            The total crypto market volume over the last 24 hours is , which
+            makes a 14.02% increase. The total volume in DeFi is currently
+            $4.83B, 2.50% of the total crypto market 24-hour volume. The volume
+            of all stable coins is now $75.16B, which is 38.90% of the total
+            crypto market 24-hour volume. Bitcoin&apos;s price is currently
+            $24,901.73. Bitcoin’s dominance is currently 68.90%, an increase of
+            0.90% over the day.
+          </Paragraph>
+        )}
       </div>
     </>
   );
