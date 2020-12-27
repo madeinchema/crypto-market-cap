85413db6d0484bbaa9f9408fd0dfbe417eab0d22
madeinchema <chemavg1@gmail.com>
2020-12-27 20:33:28 +0100

    add links and breaks to FooterContainer

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -10,6 +10,43 @@ const FooterContainer = () => {
         </Footer.Col>
       </Footer.Row>
 
+      <Footer.Break className="footer--break" />
+
+      <Footer.Row justify="space-between">
+        <Footer.Col span={8}>
+          <Footer.Title level={4} className="footer--title">
+            Products
+          </Footer.Title>
+          <Footer.Link href="#">Cryptocurrencies</Footer.Link>
+          <Footer.Link href="#">Exchanges</Footer.Link>
+          <Footer.Link href="#">Compare</Footer.Link>
+          <Footer.Link href="#">Converter</Footer.Link>
+          <Footer.Link href="#">Repository</Footer.Link>
+        </Footer.Col>
+        <Footer.Col span={8}>
+          <Footer.Title level={4} className="footer--title">
+            Development
+          </Footer.Title>
+          <Footer.Link href="#">React</Footer.Link>
+          <Footer.Link href="#">CoinGecko API</Footer.Link>
+          <Footer.Link href="#">Ant Design</Footer.Link>
+          <Footer.Link href="#">Axios</Footer.Link>
+          <Footer.Link href="#">React Virtualized</Footer.Link>
+        </Footer.Col>
+        <Footer.Col span={8}>
+          <Footer.Title level={4} className="footer--title">
+            Other projects
+          </Footer.Title>
+          <Footer.Link href="#">Todolist</Footer.Link>
+          <Footer.Link href="#">Finance Tracker</Footer.Link>
+          <Footer.Link href="#">Pomodoro App</Footer.Link>
+          <Footer.Link href="#">Simple Calculator</Footer.Link>
+          <Footer.Link href="#">Personal Portfolio</Footer.Link>
+        </Footer.Col>
+      </Footer.Row>
+
+      <Footer.Break className="footer--break" />
+
       <Footer.Row justify="space-between">
         <Footer.Col>
           <Footer.Title level={5} className="footer--title">
