c2c82f79b5b7f6a2f4f2837928f29401620f77d6
madeinchema <chemavg1@gmail.com>
2020-12-27 20:32:33 +0100

    add Link and Break elements to Footer compound comp

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,23 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
+import styled from 'styled-components';
 import { Layout, Typography, Row, Col } from 'antd';
+import colors from '../../theme/colors.module.scss';
 
-const { Title } = Typography;
+const { Title, Link } = Typography;
+
+const StyledLink = styled(Link)`
+  display: block;
+  font-weight: 500;
+  font-size: 1rem;
+  &.ant-typography {
+    color: ${colors.geekblue6};
+    &:hover {
+      color: ${colors.geekblue5};
+    }
+  }
+`;
+
 const Footer = ({ children, ...restProps }) => {
   return <Layout.Footer {...restProps}>{children}</Layout.Footer>;
 };
@@ -16,18 +32,22 @@ Footer.Col = function FooterCol({ children, ...restProps }) {
 
 Footer.Logo = function FooterLogo({ children, ...restProps }) {
   return (
-    <Title level={3} className="footer--logo" {...restProps}>
+    <Title level={3} {...restProps}>
       {children}
     </Title>
   );
 };
 
 Footer.Title = function FooterTitle({ children, ...restProps }) {
-  return (
-    <Title className="footer--title" {...restProps}>
-      {children}
-    </Title>
-  );
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Footer.Link = function FooterLink({ children, ...restProps }) {
+  return <StyledLink {...restProps}>{children}</StyledLink>;
+};
+
+Footer.Break = function FooterBreak({ children, ...restProps }) {
+  return <div {...restProps}>{children}</div>;
 };
 
 export default Footer;
