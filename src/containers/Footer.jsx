47d94889cbe69b2d5d992e9889b3d4afad770bad
madeinchema <chemavg1@gmail.com>
2020-12-27 17:38:53 +0100

    add Logo to FooterContainer

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -6,6 +6,12 @@ const FooterContainer = () => {
     <Footer className="footer">
       <Footer.Row justify="space-between">
         <Footer.Col>
+          <Footer.Logo>Crypto Market Cap</Footer.Logo>
+        </Footer.Col>
+      </Footer.Row>
+
+      <Footer.Row justify="space-between">
+        <Footer.Col>
           <Footer.Title level={5} className="footer--title">
             © 2021 Crypto Market Cap. All rights reserved.
           </Footer.Title>
