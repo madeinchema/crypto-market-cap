1cddd9adb308c3539173698bea6a2de541dedab3
madeinchema <chemavg1@gmail.com>
2020-12-28 21:25:52 +0100

    add disabled key to headerMenuData

--- src/containers/Footer/utils/constants/headerMenuData.js
+++ src/containers/Footer/utils/constants/headerMenuData.js
@@ -3,26 +3,31 @@ const headerMenuData = [
     id: 'cryptocurrencies',
     label: 'Cryptocurrencies',
     href: '/cryptocurrencies',
+    disabled: false,
   },
   {
     id: 'exchanges',
     label: 'Exchanges',
     href: '/exchanges',
+    disabled: true,
   },
   {
     id: 'compare',
     label: 'Compare',
     href: '/compare',
+    disabled: true,
   },
   {
     id: 'converter',
     label: 'Converter',
     href: '/converter',
+    disabled: true,
   },
   {
     id: 'repository',
     label: 'Repository',
     href: '/repository',
+    disabled: true,
   },
 ];
 
