ee6c91aa6e27faa1726331ba6629106166121d6f
madeinchema <chemavg1@gmail.com>
2020-12-28 18:55:09 +0100

    add headerMenuData file as a HeaderContainer constant

---
+++ src/containers/Footer/utils/constants/headerMenuData.js
@@ -0,0 +1,29 @@
+const headerMenuData = [
+  {
+    id: 'cryptocurrencies',
+    label: 'Cryptocurrencies',
+    href: '/cryptocurrencies',
+  },
+  {
+    id: 'exchanges',
+    label: 'Exchanges',
+    href: '/exchanges',
+  },
+  {
+    id: 'compare',
+    label: 'Compare',
+    href: '/compare',
+  },
+  {
+    id: 'converter',
+    label: 'Converter',
+    href: '/converter',
+  },
+  {
+    id: 'repository',
+    label: 'Repository',
+    href: '/repository',
+  },
+];
+
+export default headerMenuData;
