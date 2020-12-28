ff2108a43804343f487581bd67f49e8b68a7e719
madeinchema <chemavg1@gmail.com>
2020-12-28 17:41:31 +0100

    add ThemeProvider with the new theme.js to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Layout } from 'antd';
+import { ThemeProvider } from 'styled-components';
 import CoinsRanking from './pages/CoinsRanking/CoinsRanking';
 import FooterContainer from './containers/Footer/Footer';
 import HeaderContainer from './containers/Header/Header';
 import './app.scss';
+import theme from './theme/theme';
 
 const { Content } = Layout;
 
@@ -13,21 +15,23 @@ const App = () => {
 
   return (
     <Router>
-      <Layout>
-        <HeaderContainer />
-        <Content className="app--content">
-          <Switch>
-            <Route exact path={['/', '/currencies']}>
-              <CoinsRanking currency={currency} />
-            </Route>
-            <Route exact path="/exchanges">
-              Exchanges
-            </Route>
-            <Route path="*">404</Route>
-          </Switch>
-        </Content>
-        <FooterContainer />
-      </Layout>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <HeaderContainer />
+          <Content className="app--content">
+            <Switch>
+              <Route exact path={['/', '/currencies']}>
+                <CoinsRanking currency={currency} />
+              </Route>
+              <Route exact path="/exchanges">
+                Exchanges
+              </Route>
+              <Route path="*">404</Route>
+            </Switch>
+          </Content>
+          <FooterContainer />
+        </Layout>
+      </ThemeProvider>
     </Router>
   );
 };
