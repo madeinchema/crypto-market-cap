1498d4fb42bd89b959e369c399a7f71ba6e341a4
madeinchema <chemavg1@gmail.com>
2020-12-28 20:52:47 +0100

    make "/cryptocurrencies" redirect to "/"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import { Layout } from 'antd';
 import { ThemeProvider } from 'styled-components';
 import CoinsRanking from './pages/CoinsRanking/CoinsRanking';
 import FooterContainer from './containers/Footer/Footer';
 import HeaderContainer from './containers/Header/Header';
+
 import './app.scss';
 import theme from './theme/theme';
 
@@ -16,15 +22,18 @@ const App = () => {
   return (
     <Router>
       <ThemeProvider theme={theme}>
+        <HeaderContainer />
         <Layout>
-          <HeaderContainer />
           <Content className="app--content">
             <Switch>
-              <Route exact path={['/', '/currencies']}>
+              <Route exact path="/">
                 <CoinsRanking currency={currency} />
               </Route>
               <Route exact path="/exchanges">
                 Exchanges
+              </Route>
+              <Route path="/cryptocurrencies">
+                <Redirect to="/" />
               </Route>
               <Route path="*">404</Route>
             </Switch>
