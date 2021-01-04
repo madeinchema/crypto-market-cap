a19989f46b5698b27deddb50f20ce00c592a5946
madeinchema <chemavg1@gmail.com>
2021-01-04 23:22:26 +0100

    add SiderContainer inside HeaderContainer

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from 'react';
 import Logo from '../../assets/Logo';
 import { Header } from '../../components';
 import headerMenuData from '../Footer/utils/constants/headerData';
+import SiderContainer from '../Sider/Sider';
 import './header.scss';
 
 const HeaderContainer = () => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const closeSider = () => {
+    setCollapsed(true);
+  };
+
+  const openSider = () => {
+    setCollapsed(false);
+  };
+
+  useEffect(() => {
+    if (collapsed) {
+      document.body.style.overflow = 'unset';
+      document.body.style.paddingRight = '0px';
+    }
+    return () => {
+      document.body.style.overflow = 'hidden';
+      document.body.style.paddingRight = '15px';
+    };
+  }, [collapsed]);
+
   return (
     <Header>
       <Header.Logo>CryptoMarketCap</Header.Logo>
@@ -11,6 +34,12 @@ const HeaderContainer = () => {
         dataSource={headerMenuData}
         siderLogo={<Logo />}
         logoText="CryptoMarketCap"
+        openSider={openSider}
+      />
+      <SiderContainer
+        dataSource={headerMenuData}
+        onCollapse={closeSider}
+        collapsed={collapsed}
       />
     </Header>
   );
