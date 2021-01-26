4b18b2dbd70025d539c458bd26d84091a8d590ac
madeinchema <chemavg1@gmail.com>
2021-01-26 23:57:21 +0100

    create Currency page file

---
+++ src/pages/Currency/Currency.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Currency = (props) => {
+  return <div>Currency</div>;
+};
+
+Currency.propTypes = {};
+
+export default Currency;
