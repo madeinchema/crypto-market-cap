6e56c5ba7932800c9c7ac10ef2df3d6b66547fa5
madeinchema <chemavg1@gmail.com>
2021-01-29 00:08:06 +0100

    add isLoading state and render Spin comp conditionally in Currency.jsx

--- src/pages/Currency/Currency.jsx
+++ src/pages/Currency/Currency.jsx
@@ -1,23 +1,29 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Spin } from 'antd';
 import { getCoinDataFromApi } from '../../utilities/api';
 
 const Currency = () => {
   const [coinData, setCoinData] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(undefined);
   const { id } = useParams();
 
   useEffect(() => {
     if (id) {
+      setIsLoading(true);
       getCoinDataFromApi(id).then((data) => setCoinData(data));
+      setIsLoading(false);
     }
   }, [id]);
 
-  return (
+  return !isLoading && coinData ? (
     <div>
       <div>
         {coinData.name} - {coinData.symbol}
       </div>
     </div>
+  ) : (
+    <Spin />
   );
 };
 
