baf17f5f8ac5a8cfa1dc6312fb647f18dd05795f
madeinchema <chemavg1@gmail.com>
2021-01-29 00:00:19 +0100

    get coin data from api in Currency & render name and symbol

--- src/pages/Currency/Currency.jsx
+++ src/pages/Currency/Currency.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getCoinData } from '../../utilities/api';
 
-const Currency = (props) => {
-  return <div>Currency</div>;
+const Currency = () => {
+  const [coinData, setCoinData] = useState(undefined);
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (id) {
+      getCoinData(id).then((data) => setCoinData(data));
+    }
+  }, [id]);
+
+  return (
+    <div>
+      <div>
+        {coinData.name} - {coinData.symbol}
+      </div>
+    </div>
+  );
 };
 
 Currency.propTypes = {};
