df0832c5881cb11982093b948942c8c5a061c9d7
madeinchema <chemavg1@gmail.com>
2021-02-01 20:04:54 +0100

    add coinPriceData elements and antd structure to Currency page

--- src/pages/Currency/Currency.jsx
+++ src/pages/Currency/Currency.jsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Space, Spin, Tag, Typography } from 'antd';
-import { getCoinDataFromApi } from '../../utilities/api';
+import { Col, Row, Space, Spin, Tag, Typography } from 'antd';
+import {
+  getCoinDataFromApi,
+  getCoinPriceDataFromApi,
+} from '../../utilities/api';
 
 const { Title } = Typography;
 
 const Currency = () => {
   const [coinData, setCoinData] = useState(undefined);
+  const [coinPriceData, setCoinPriceData] = useState(undefined);
   const [isLoading, setIsLoading] = useState(undefined);
   const { id } = useParams();
 
@@ -24,20 +28,41 @@ const Currency = () => {
     if (id) {
       setIsLoading(true);
       getCoinDataFromApi(id).then((data) => setCoinData(data));
+      getCoinPriceDataFromApi(id).then((data) => setCoinPriceData(data));
       setIsLoading(false);
     }
   }, [id]);
 
-  return !isLoading && coinData ? (
-    <Space align="center">
-      <img src={coinData.image.small} alt="coin icon" />
-      <Space align="center">
-        <Title level={2} style={currencyStyles.title}>
-          {coinData.name}
-        </Title>
-        <Tag style={currencyStyles.tag}>{coinData.symbol.toUpperCase()}</Tag>
-      </Space>
-    </Space>
+  return !isLoading && coinData && coinPriceData ? (
+    <Row>
+      <Col span={24}>
+        <Space align="center">
+          <img src={coinData.image.small} alt="coin icon" />
+          <Title level={2} style={currencyStyles.title}>
+            {coinData.name}
+          </Title>
+          <Tag style={currencyStyles.tag}>{coinData.symbol.toUpperCase()}</Tag>
+        </Space>
+      </Col>
+
+      <Col span={24}>
+        <Space align="center">
+          <Title level={2} style={currencyStyles.title}>
+            {coinPriceData.usdMarketCap}
+          </Title>
+        </Space>
+      </Col>
+
+      <Col span={24}>
+        <Space align="center" direction="vertical">
+          <Title style={currencyStyles.tag}>{coinPriceData.usd24hVol}</Title>
+          <Title style={currencyStyles.tag}>{coinPriceData.usd24hChange}</Title>
+          <Title style={currencyStyles.tag}>
+            {coinPriceData.lastUpdatedAt}
+          </Title>
+        </Space>
+      </Col>
+    </Row>
   ) : (
     <Spin />
   );
