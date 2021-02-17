0026bb8d29c9c8b0965172249a8324d4097adf74
madeinchema <chemavg1@gmail.com>
2021-02-17 11:12:42 +0100

    add time frame picker with 30d as default

--- src/pages/Currency/components/CurrencyPriceChart.jsx
+++ src/pages/Currency/components/CurrencyPriceChart.jsx
@@ -1,33 +1,73 @@
 import { useEffect, useState } from 'react';
-import { Row, Col, Spin } from 'antd';
+import { Row, Col, Spin, Radio } from 'antd';
 import { Stock } from '@ant-design/charts';
 import { useParams } from 'react-router-dom';
 import { getCoinChartDataFromApi } from '../../../utilities/api';
 import '../currency.scss';
 
+const TIME_FRAMES = {
+  oneYear: {
+    daysQty: 365,
+    label: '1y',
+  },
+  thirtyDays: {
+    daysQty: 30,
+    label: '30d',
+  },
+  oneDay: {
+    daysQty: 1,
+    label: '24hs',
+  },
+};
+
 const CurrencyPriceChart = () => {
+  const [timeFrame, setTimeFrame] = useState(TIME_FRAMES.thirtyDays);
   const [coinChartData, setCoinChartData] = useState(undefined);
   const [isLoading, setIsLoading] = useState(undefined);
-  const { id } = useParams();
+  const { id: coinId } = useParams();
 
   useEffect(() => {
-    if (id) {
+    if (coinId) {
       setIsLoading(true);
-      getCoinChartDataFromApi(id).then((data) => setCoinChartData(data));
+      getCoinChartDataFromApi(coinId, timeFrame.daysQty).then((data) =>
+        setCoinChartData(data)
+      );
       setIsLoading(false);
     }
-  }, [id]);
+  }, [coinId, timeFrame.daysQty]);
+
+  const options = [
+    { label: '1y', value: '1y' },
+    { label: '30d', value: '30d' },
+    { label: '24hs', value: '24hs' },
+  ];
 
   return !isLoading && coinChartData ? (
     <Row>
       <Col span={24}>
-        <Stock
-          width="100%"
-          height={400}
-          data={coinChartData}
-          xField="trade_date"
-          yField={['open', 'close', 'high', 'low']}
-        />
+        <Row justify="end">
+          <Col>
+            <Radio.Group
+              options={options}
+              optionType="button"
+              buttonStyle="solid"
+              value={timeFrame.label}
+            />
+          </Col>
+        </Row>
+      </Col>
+      <Col span={24}>
+        <Row>
+          <Col span={24}>
+            <Stock
+              width="100%"
+              height={400}
+              data={coinChartData}
+              xField="trade_date"
+              yField={['open', 'close', 'high', 'low']}
+            />
+          </Col>
+        </Row>
       </Col>
     </Row>
   ) : (
