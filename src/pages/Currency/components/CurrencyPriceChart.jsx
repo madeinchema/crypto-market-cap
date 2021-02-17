0a6e218eac189dfebce3ffe06296c2736ec946f0
madeinchema <chemavg1@gmail.com>
2021-02-17 11:21:20 +0100

    handle time frame change in CurrencyPriceChart

--- src/pages/Currency/components/CurrencyPriceChart.jsx
+++ src/pages/Currency/components/CurrencyPriceChart.jsx
@@ -6,22 +6,22 @@ import { getCoinChartDataFromApi } from '../../../utilities/api';
 import '../currency.scss';
 
 const TIME_FRAMES = {
-  oneYear: {
+  '1y': {
     daysQty: 365,
     label: '1y',
   },
-  thirtyDays: {
+  '30d': {
     daysQty: 30,
     label: '30d',
   },
-  oneDay: {
+  '24hs': {
     daysQty: 1,
     label: '24hs',
   },
 };
 
 const CurrencyPriceChart = () => {
-  const [timeFrame, setTimeFrame] = useState(TIME_FRAMES.thirtyDays);
+  const [timeFrame, setTimeFrame] = useState(TIME_FRAMES['30d']);
   const [coinChartData, setCoinChartData] = useState(undefined);
   const [isLoading, setIsLoading] = useState(undefined);
   const { id: coinId } = useParams();
@@ -36,10 +36,15 @@ const CurrencyPriceChart = () => {
     }
   }, [coinId, timeFrame.daysQty]);
 
+  const handleTimeFrame = (event) => {
+    const selectedTimeFrame = event.target.value;
+    setTimeFrame(TIME_FRAMES[selectedTimeFrame]);
+  };
+
   const options = [
-    { label: '1y', value: '1y' },
-    { label: '30d', value: '30d' },
     { label: '24hs', value: '24hs' },
+    { label: '30d', value: '30d' },
+    { label: '1y', value: '1y' },
   ];
 
   return !isLoading && coinChartData ? (
@@ -52,6 +57,7 @@ const CurrencyPriceChart = () => {
               optionType="button"
               buttonStyle="solid"
               value={timeFrame.label}
+              onChange={handleTimeFrame}
             />
           </Col>
         </Row>
